5a5cc701aa595203da2ef63009f311f60aab9fd4
Your Name <you@example.com>
2023-08-13 23:43:03 -0300

    vai

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ $(document).ready(function(){
       margin: 10,
     });
   });
+
+  function redirecionar() {
+    window.location.href = "https://api.whatsapp.com/send?phone=558184428537&text=Ol%C3%A1,%20Fisioterapeuta%20Rayssa%20Muniz,%20quero%20saber%20mais%20sobre%20as%20consultas%20e%20agendamentos!!";
+}
