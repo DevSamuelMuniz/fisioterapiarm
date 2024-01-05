2f2ad23411cc041bcc9af7b6bf90c7fcc6f67e99
DevSamuelMuniz <devsamuelmuniz@hotmail.com>
2024-01-05 20:49:20 -0300

    att

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-      items: 3,
-      margin: 10,
-      loop: true,
-      nav: true,
-      navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
-    });
-  });
-
-  function redirecionar() {
-    window.location.href = "https://calendly.com/fisioterapiarm/agendamento";
-}
+  $('.owl-carousel').owlCarousel({
+    items: 4,
+    margin: 10,
+    loop: true,
+    responsive: {
+    0: {
+      items: 2
+  },
+    
+    1339: {
+      items: 4
+    }
+  }
+});
+});
